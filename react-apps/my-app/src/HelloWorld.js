28b7c0a792e7adcc3a072e6c394f0dd8d5761217
BugsFounder <bugsfounder2021@gmail.com>
2024-07-13 13:43:44 +0530

    Hello World using functional components

---
+++ react-apps/my-app/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+function HelloWorld() {
+    return (
+        // <Fragment></Fragment> and <></> are some 
+        <Fragment>
+            <hr />
+            <h1>Hello, World!</h1>
+        </Fragment>
+    )
+}
+
+export default HelloWorld;
