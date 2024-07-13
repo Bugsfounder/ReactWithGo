4773a926c8222c4c446c363e292a517212045b04
BugsFounder <bugsfounder2021@gmail.com>
2024-07-13 17:56:01 +0530

    states using class component

--- react-apps/my-app/src/index.js
+++ react-apps/my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloWorld from './HelloWorld';
+import AppClass from './AppClass';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="container">
       <div className="row">
         <div className="col">
-          <HelloWorld msg="Hello, again!" />
+          <AppClass msg="Hello, again!" />
         </div>
       </div>
     </div>
