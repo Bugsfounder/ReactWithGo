ffda593d466355995a4fd49c96d3a864b0f0f7ec
BugsFounder <bugsfounder2021@gmail.com>
2024-07-15 12:07:15 +0530

    Creating Layout of Application

---
+++ react-apps/go-movies-front-end/src/components/Movies.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Movies = () => {
+    return (
+        <>
+            <div className="text-center">
+                <h2>Movies</h2>
+                <hr />
+            </div>
+        </>
+    )
+}
+
+export default Movies
