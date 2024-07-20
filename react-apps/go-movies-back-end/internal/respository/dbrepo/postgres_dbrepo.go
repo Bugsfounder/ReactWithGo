86c67c761c2beb4fe5f725b54dcd8976278d655a
BugsFounder <bugsfounder2021@gmail.com>
2024-07-20 09:55:44 +0530

    fixed query issue

--- react-apps/go-movies-back-end/internal/respository/dbrepo/postgres_dbrepo.go
+++ react-apps/go-movies-back-end/internal/respository/dbrepo/postgres_dbrepo.go
@@ -69,9 +69,9 @@ func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
-	
+
 	query := `select id, email, first_name, last_name, password, 
-			created_at, update_at from users where email = $1`
+			created_at, updated_at from users where email = $1`
 
 	var user models.User
 	row := m.DB.QueryRowContext(ctx, query, email)
